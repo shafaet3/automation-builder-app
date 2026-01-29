596cebd7a60195147b8bb17277fb82d3dc856fee
Shafaet <shafaet3@gmail.com>
2026-01-30 02:36:47 +0600

    Update CORS configuration to allow OPTIONS method

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ import cors from "cors";
 import dotenv from "dotenv";
 import automationRoutes from "./routes/automationRoutes.js";
 import emailRoutes from "./routes/emailRoutes.js";
-import cors from "cors";
 dotenv.config();
 
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "*" }));
-app.use(express.json());
-
 app.use(cors({
   origin: [
     "http://localhost:3000",
     "https://automation-builder-frontend.vercel.app"
   ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
+
+app.options("*", cors());
+app.use(express.json());
 
 // Routes
 app.use("/automations", automationRoutes);
